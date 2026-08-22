2deb3ed1cc0918e90ce7b87b1a018be998d43864
yashvantsolanki25 <yashvantsolanki25@gmail.com>
2026-08-22 12:51:40 +0530

    method ridding

--- samples.java
+++ samples.java
@@ -7,6 +7,7 @@ class samples
         System.out.println(jane);
         } */
        // Composing
+       /*
 record Population(int population) {}
 record City(String name, Population population) {
     // static methods are allowed in records
@@ -17,6 +18,27 @@ record City(String name, Population population) {
 }
 
 var paris = City.of("Paris", 2_161);
-System.out.println(paris);
+System.out.println(paris); */
+// method overriding
+record City(String name) {
+
+    public boolean equals(Object other) {
+        return other instanceof City(String name) &&
+                this.name.equalsIgnoreCase(name);
+    }
+
+    public int hashCode() {
+        return name != null ? name.toUpperCase().hashCode() : 0;
+    }
+}
+
+var paris1 = new City("Paris");
+var paris2 = new City("paris");
+var paris3 = new City("PARIS");
+System.out.println("1 == 2 ? " + paris1.equals(paris2));
+System.out.println("2 == 3 ? " + paris2.equals(paris3));
+System.out.println("1 == 3 ? " + paris1.equals(paris3));
+
+
     }
 }
