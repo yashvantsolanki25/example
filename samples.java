9d02ca40854580b8a4b010a59d72942a07a09eaf
yashvantsolanki25 <yashvantsolanki25@gmail.com>
2026-08-22 12:22:34 +0530

    Composing

--- samples.java
+++ samples.java
@@ -2,8 +2,21 @@ class samples
 {
     public static void main(String args[])
     {
-        record player(String last, String first, int level) {}
+       /* record player(String last, String first, int level) {}
         var jane = new player("Doe" , "Jane" , 1);
         System.out.println(jane);
-        }
+        } */
+       // Composing
+record Population(int population) {}
+record City(String name, Population population) {
+    // static methods are allowed in records
+    public static City of(String name, int p) {
+        var population = new Population(p);
+        return new City(name, population);
     }
+}
+
+var paris = City.of("Paris", 2_161);
+System.out.println(paris);
+    }
+}
