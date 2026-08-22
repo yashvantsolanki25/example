e6ab6ac8760c45bd6c2deac3c30b0c477630b777
yashvantsolanki25 <yashvantsolanki25@gmail.com>
2026-08-22 14:03:47 +0530

    BiFunction with Unnamed Pattern

--- unp.java
+++ unp.java
@@ -1,5 +1,6 @@
-import java.util.List;
-import java.util.function.Function;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.IntStream;
 class unp
 {
     public static void main(String[] args)
@@ -10,10 +11,18 @@ Consumer<String> notInterested = _ -> System.out.println("I'm not interested in 
 strings.forEach(notInterested);
 */
      // Function with Unnamed Pattern
+     /*
 List<String> strings = List.of("1", "11", "111");
 Function<String, Integer> constantLength = _ -> 3;
 var result = strings.stream()
         .map(constantLength)
+        .toList();
+System.out.println("result = " + result); */
+        // BiFunction with Unnamed Pattern
+var strings = List.of("one", "two", "three", "four");
+BiFunction<String, Integer, Integer> indexer = (_, i) -> i;
+var result = IntStream.range(0, strings.size())
+        .mapToObj(index -> indexer.apply(strings.get(index), index))
         .toList();
 System.out.println("result = " + result);
 
